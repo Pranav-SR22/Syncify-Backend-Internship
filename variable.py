c7013f3d38e166f5e488dd320e4b8eb73bb3423b
Havok22 <pranav20s02raj@gmail.com>
2025-12-04 21:15:31 +0530

    2

---
+++ variable.py
@@ -0,0 +1,13 @@
+#Storing string in variable
+name = "Pranav"
+print(name)
+#storing number in variable
+num = 25
+print(num)
+#case sesititvity
+name="p"
+Name="q"
+NAME="r"
+print(name) #p
+print(Name) #q
+print(NAME) #r
